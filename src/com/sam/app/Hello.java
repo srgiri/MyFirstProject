0186614c2512712199375b89242000d822ab686d
srgiri <saumya.giri07@gmail.com>
2019-09-04 23:27:17 +0530

    TDS deducted from every employee

--- src/com/sam/app/Hello.java
+++ src/com/sam/app/Hello.java
@@ -8,6 +8,9 @@ public class Hello {
         Employee e2 = new Employee(2,"Deepak", 30000);
 
         int totalTax = (int)((e1.getEmpSalary() + e2.getEmpSalary()) * 0.25);
+
+        e1.setEmpSalary(e1.getEmpSalary() - (int)(e1.getEmpSalary() * 0.15));
+        e2.setEmpSalary(e2.getEmpSalary() - (int)(e2.getEmpSalary() * 0.15));
         System.out.println("Total tax paid: "+totalTax);
     }
 }
