f91bf3e2055d87a82ac5a5954c8a156af3733236
srgiri <saumya.giri07@gmail.com>
2019-09-05 20:46:09 +0530

    Employee name is printed

--- src/com/sam/app/Hello.java
+++ src/com/sam/app/Hello.java
@@ -2,6 +2,8 @@ package com.sam.app;
 
 import com.sam.model.Employee;
 
+import javax.xml.bind.SchemaOutputResolver;
+
 public class Hello {
     public static void main(String args[]) {
         Employee e1 = new Employee(1,"Saumya", 20000);
@@ -12,5 +14,6 @@ public class Hello {
         e1.setEmpSalary(e1.getEmpSalary() - (int)(e1.getEmpSalary() * 0.1));
         e2.setEmpSalary(e2.getEmpSalary() - (int)(e2.getEmpSalary() * 0.1));
         System.out.println("Total tax paid: "+totalTax);
+        System.out.println("Employee name: "+e1.getEmpName());
     }
 }
